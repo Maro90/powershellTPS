8d8134251792589df3ef537d3a98c44a6f6e69e8
Mauro Gonzalez <mauro.d.gonzalez90@gmail.com>
2018-04-10 11:41:38 -0300

    Change: updated all the answers

--- TP1/tp1.ps1
+++ TP1/tp1.ps1
@@ -12,12 +12,26 @@ Write-Output $contador
 El objetivo del script es contar todos archivos que contienen un número en el nombre completo, 
 de todos directorios y subdirectorios del path pasado como parámetro
 
+
 2.
 Se prodría validar:
     - que se pase un parámetro
     - que el parámetro sea un string
     - se podría validar que el string tenga una longitud maxima o mínima
 
-3. El operando "-match" sirve para búscar la ocurrencia de una expresión regular, en este caso particular se busca que haya un número.
+
+
+3. 
+El operando "-match" sirve para búscar la ocurrencia de una expresión regular, en este caso particular se busca que haya un número.
+
+
+
+4.
+Se podría reemplazar el foreach de la siguiente manera
+
+Param($path)
+$contador = Get-ChildItem $path | Where-Object {$_.FullName -match '[2-9]'} | measure | % {$_.Count}
+Write-Output $contador
+
 
 #>
