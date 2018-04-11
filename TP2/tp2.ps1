36510e6a3492ac2ede344f4f68fd1c0898663de6
Mauro Gonzalez <mauro.d.gonzalez90@gmail.com>
2018-04-11 12:01:06 -0300

    Add: tp2.ps1

---
+++ TP2/tp2.ps1
@@ -0,0 +1,31 @@
+
+Param(
+    [parameter(Mandatory=$true)]
+    [validateNotNullorEmpty()]
+    [validateLenght(1,50)]
+    [string]$path
+)
+<#
+.Synopsis
+   This script checks the 
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+.INPUTS
+   Inputs to this cmdlet (if any)
+.OUTPUTS
+Output from this cmdlet (if any)
+.NOTES
+   General notes
+.COMPONENT
+   The component this cmdlet belongs to
+.ROLE
+   The role this cmdlet belongs to
+.FUNCTIONALITY
+   The functionality that best describes this cmdlet
+
+
+#>
