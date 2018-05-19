9588f82631dec276d10d1028bea56e58a5aec160
Juan Arana <arana.juanpablo@gmail.com>
2018-05-18 22:02:29 -0300

    EJ2

--- TP2/Ejercicio2/Ejercicio2.sh
+++ TP2/Ejercicio2/Ejercicio2.sh
@@ -1,1 +1,35 @@
-#WIP
+#!/bin/bash
+
+if [ $# != 2 ]; then
+    echo "Debe pasar dos archivos por parametro"
+    exit
+fi
+if ! [ -f "$1" ] || ! [ -f "$2" ]; then
+    echo "Archivos invalidos"
+    exit 
+fi
+file -i "$1" | grep text/plain >> /dev/null
+if [ $? != 0 ]
+then
+    echo "El archivo esta vacio"
+    exit 
+fi
+cp $1 $1.bak
+
+ARCH1=/tmp/`basename $1`.$$
+
+cp $1 $ARCH1
+
+PMAYUS=$(sed -r 's/(.*)/s\/\1\/\1\/I;/' $2)
+REPLACE="
+s/[A-Z]*/\L&/g
+s/^./\u&/
+s/([\.;,])\s*([a-z])/\1 \2/g
+s/(\s+)/ /g
+s/\.\s+$/./
+$PMAYUS
+"
+
+sed -i -r "$REPLACE" $ARCH1
+
+mv $ARCH1 $1
