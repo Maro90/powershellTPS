7d66fdc788f14553816c3d92bc5e1487840ac715
Nicolas Sapaya <nicolas.sapaya@gmail.com>
2018-05-23 15:18:41 -0300

    Add Ej2: Encabezado y Casos de Prueba

--- TP2/Ejercicio2/Ejercicio2.sh
+++ TP2/Ejercicio2/Ejercicio2.sh
@@ -1,7 +1,49 @@
 #!/bin/bash
 
+#################################################
+#			  Sistemas Operativos			 	#
+#		Trabajo Práctico 2 - Ejericio 2			#
+#		Nombre del Script: Ejercicio2.sh		#
+#												#
+#				Integrantes:					#
+#		Arana, Juan Pablo 		33904497		#
+#		Gonzalez, Mauro Daniel 	35368160		#
+#		Sapaya, Nicolás Martín 	38319489		#
+#												#
+#		Instancia de Entrega: Entrega			#
+#												#
+#################################################
+
+#	Funcion si se ingresaro parametros incorrectos.
+ErrorSintaxOHelp() { 
+	clear
+	#	Si $1 es 1, entonces es error de sintaxis
+	if test $1 != 0; then
+		echo 'Error. La sintaxis del script es la siguiente:'
+		echo 'Para ayuda:'
+		echo "$0 -h"
+		echo "$0 -?"
+		echo "$0 -help"
+		echo "----------------------------------"
+	else
+		echo 'Help:'
+		echo "La sintaxis del script es la siguiente:"
+	fi
+	echo "$0 [Archivo][Archivo]"
+	exit	
+}
+
+#	Es ayuda ?
+if [ "$1" = "-h" ]; then
+	ErrorSintaxOHelp 0
+elif [ "$1" = "-help" ]; then
+	ErrorSintaxOHelp 0
+elif [ "$1" = "-?" ]; then
+	ErrorSintaxOHelp 0
+fi
+
 if [ $# != 2 ]; then
-    echo "Debe pasar dos archivos por parametro"
+    ErrorSintaxOHelp 1
     exit
 fi
 if ! [ -f "$1" ] || ! [ -f "$2" ]; then
@@ -14,13 +56,13 @@ then
     echo "El archivo esta vacio"
     exit 
 fi
-cp $1 $1.bak
+cp "$1" "$1.bak"
 
-ARCH1=/tmp/`basename $1`.$$
+ARCH1=/tmp/`basename "$1"`.$$
 
-cp $1 $ARCH1
+cp "$1" $ARCH1
 
-PMAYUS=$(sed -r 's/(.*)/s\/\1\/\1\/I;/' $2)
+PMAYUS=$(sed -r 's/(.*)/s\/\1\/\1\/I;/' "$2")
 REPLACE="
 s/[A-Z]*/\L&/g
 s/^./\u&/
@@ -32,4 +74,4 @@ $PMAYUS
 
 sed -i -r "$REPLACE" $ARCH1
 
-mv $ARCH1 $1
+mv "$ARCH1" "$1"
