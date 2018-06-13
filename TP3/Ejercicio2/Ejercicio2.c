82f6af134e0dae15fbdede856971162add901a71
Nicolas Sapaya <nicolas.sapaya@gmail.com>
2018-06-13 13:14:20 -0300

    progreso ej2

--- TP3/Ejercicio2/Ejercicio2.c
+++ TP3/Ejercicio2/Ejercicio2.c
@@ -5,6 +5,7 @@
 #include <time.h>
 #include <ctype.h>
 #include <string.h>
+#include <dirent.h>
 
 #define ES_LETRA(x) ((x > 64 && x < 91) || (x > 96 && x < 123)) ? 1 : 0
 #define ES_VOCAL(x) (tolower(x) == 97 || tolower(x) == 101 || tolower(x) == 105 || tolower(x) == 111 || tolower(x) == 118) ? 1 : 0
@@ -28,8 +29,39 @@ int main(int argc, char const *argv[]){
     return 0;
 }
 
+int countFiles(){
+    int file_count = 0;
+    DIR * dirp;
+    struct dirent * entry;
+
+    dirp = opendir("Entrada/"); /* There should be error handling after this */
+    
+    if(!dirp){
+        printf("No se pudo abrir el directorio.\n");
+        exit(1);
+    }
+
+    entry = readdir(dirp);
+    printf("%s\n", entry->d_name);
+    
+    while(!entry) {
+        if(entry->d_type == DT_REG){ /* If the entry is a regular file */
+            file_count++;
+        }
+        entry = readdir(dirp);
+    }
+    closedir(dirp);
+}
+void printFile(FILE* pf, struct tm* tInfo1, int pid, struct tm* tInfo2, int cVocales, int cConsonantes, int cCaracteres){
+    fprintf(pf, "%d:%d:%d\n", tInfo1->tm_hour, tInfo1->tm_min, tInfo1->tm_sec);
+    fprintf(pf, "PID: %d\n",pid);
+    fprintf(pf ,"Vocales:%d\nConsonantes:%d\nCaracteres:%d\n", cVocales, cConsonantes, cCaracteres);
+    fprintf(pf, "%d:%d:%d\n", tInfo2->tm_hour, tInfo2->tm_min, tInfo2->tm_sec);
+}
+
 void* analyze(char* name){
-    printf("%d",getpid());
+    printf("PID THREAD: %d\n",getpid());
+    printf("FILES = %d\n",countFiles());
     FILE *fp;
     FILE *fout;
     char caracter;
@@ -56,11 +88,9 @@ void* analyze(char* name){
         exit(1);
     }
 
-    // Tomo el tiempo de inicio y lo imprimo en el file
+    // Tomo el tiempo de inicio
     time(&timeBegin);
     timeInfoB = localtime(&timeBegin);
-
-    fprintf(fout, "%d:%d:%d\n", timeInfoB->tm_hour, timeInfoB->tm_min, timeInfoB->tm_sec);
     ///////////////////////////////////
 
     while((caracter = fgetc(fp)) != EOF){
@@ -68,10 +98,8 @@ void* analyze(char* name){
         printf("%c",caracter);
         // es letra?
         if(ES_LETRA(caracter)){
-        //if((caracter > 64 && caracter < 91) || (caracter > 96 && caracter < 123)){
              // Es vocal o consontante?
             if(ES_VOCAL(caracter)){
-            //if(tolower(caracter) == 97 || tolower(caracter) == 101 || tolower(caracter) == 105 || tolower(caracter) == 111 || tolower(caracter) == 118){
                 // Es vocal
                 countVocales++;
             }else{
@@ -88,14 +116,16 @@ void* analyze(char* name){
     printf("\n-------------------------------------------\n");
     printf("Vocales:%d\nConsonantes:%d\nCaracteres:%d\n", countVocales, countConsonantes, countCaracteres);
 
-    // Tomo el tiempo de finalización y lo imprimo en el file
+    // Tomo el tiempo de finalización
     time(&timeEnd);
     timeInfoE = localtime(&timeEnd);
 
-    fprintf(fout ,"Vocales:%d\nConsonantes:%d\nCaracteres:%d\n", countVocales, countConsonantes, countCaracteres);
-    fprintf(fout, "%d:%d:%d\n", timeInfoE->tm_hour, timeInfoE->tm_min, timeInfoE->tm_sec);
+    // Imprimo todo el file
+    printFile(fout, timeInfoB, getpid(), timeInfoE, countVocales, countConsonantes, countCaracteres);
 
     // Cierro file pointers
     fclose (fout);
     fclose (fp);
+
+    return(0);
 }
