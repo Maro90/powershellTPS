8779a852162f0f02308ac842eda095b59df1e317
Mauro <mauro.d.gonzalez90@gmail.com>
2018-06-20 19:37:26 -0300

    Lista dinamica de las preguntas

--- TP3/Ejercicio4/Servidor.c
+++ TP3/Ejercicio4/Servidor.c
@@ -9,24 +9,27 @@
 
 typedef struct pregunta{
     char* pregunta;
-    char* respuesta1;
-    char* respuesta2;
-    char* respuesta3;
-    char* respuesta4;
+    char* respuestas[4];
     int rc;
     struct pregunta * siguiente;
 }t_pregunta;
 
-t_pregunta *lista_preguntas;
+t_pregunta *lista_preguntas = NULL;
 
-void abrirArchivo();
+void abrirArchivoDePreguntas();
+void agregarPreguntaALista(char * pregunta, char * respuestas[], int respuestaCorrecta);
 
-int main(int argc, char *const argv[]){
-    abrirArchivo();
+int main(int argc, char *const argv[]) {
+    abrirArchivoDePreguntas();
     exit(EXIT_SUCCESS);
 }
 
-void abrirArchivo(){
+//---------------------------------------------------------------------------------------------------
+/*
+*   Se encarga de leer el archivo de preguntas y cargar la lista.
+*
+*/
+void abrirArchivoDePreguntas() {
     int cantidadDePreguntas = 0;
     int nroRespuesta = 0;
 
@@ -35,9 +38,6 @@ void abrirArchivo(){
     size_t len = 0;
     ssize_t read;
 
-    lista_preguntas = malloc(sizeof(t_pregunta));
-    lista_preguntas->siguiente = NULL;
-
     fp = fopen("preguntas.txt", "r");
     if (fp == NULL) {
         exit(EXIT_FAILURE);
@@ -45,48 +45,75 @@ void abrirArchivo(){
 
     t_pregunta *preguntas = lista_preguntas;
 
+    int respuestasCorrectas = 0;
+    int respuestaCorrecta = -1;
+    char* resp[4];
+    char* preg;
+
+    int preguntaValida = 0;
+
     while ((read = getline(&line, &len, fp)) != -1) {
-
+        
+        //Obtengo el primer caracter, para saber si es pregunta o respuesta.
         char type = line[0];
+        
         if( type == 'P'){
-            if(cantidadDePreguntas > 0){
-                //Agrego nueva pregunta a la lista
-                preguntas->siguiente = malloc(sizeof(t_pregunta));
-                preguntas->siguiente->siguiente = NULL;
-                preguntas = preguntas->siguiente;
+            //Siempre esta la pregunta antes de las respuestas.
+            if( nroRespuesta != 4 || respuestasCorrectas != 1) {
+                preguntaValida = 0;
             }
+            if (preguntaValida) {
 
-            cantidadDePreguntas++;
+                //Si hubo una pregunta anterior y es válida, se debe agregar a la lista.
+                agregarPreguntaALista(preg,resp,respuestaCorrecta);
+                cantidadDePreguntas++;
+                
+                //Libero los punteros
+                free(preg);
+                for(int i=0; i<4;i++) {
+                    free(resp[i]);
+                }
+            }
+            respuestaCorrecta = -1;
+            respuestasCorrectas = 0;
             nroRespuesta = 0;
-            preguntas->pregunta = (char*)malloc(strlen(line + 2) * sizeof(char));
-            strcpy(preguntas->pregunta,line + 2);
-        } else {
-            int offset = 2;
-            if( line[1] == 'C'){    //Respuesta Correcta
-                preguntas->rc = nroRespuesta;
-                offset = 3;
-            }
+            preguntaValida = 1;
+            preg = (char*)malloc(strlen(line + 2) * sizeof(char));
+            strcpy(preg,line + 2);
 
-            switch(nroRespuesta){
-                case 0:
-                    preguntas->respuesta1 = (char*)malloc(strlen(line + offset) * sizeof(char));
-                    strcpy(preguntas->respuesta1,line + offset);
-                    break;
-                case 1:
-                    preguntas->respuesta2 = (char*)malloc(strlen(line + offset) * sizeof(char));
-                    strcpy(preguntas->respuesta2,line + offset);
-                    break;
-                case 2:
-                    preguntas->respuesta3 = (char*)malloc(strlen(line + offset) * sizeof(char));
-                    strcpy(preguntas->respuesta3,line + offset);
-                    break;
-                case 3:
-                    preguntas->respuesta4 = (char*)malloc(strlen(line + offset) * sizeof(char));
-                    strcpy(preguntas->respuesta4,line + offset);
-                    break;
+        } else if ( type == 'R' ) {
+            if(nroRespuesta > 3){
+                preguntaValida = 0;
+            } else {
+                int offset = 2;
+                if( line[1] == 'C') {    //Respuesta Correcta
+                    respuestaCorrecta = nroRespuesta;
+                    respuestasCorrectas++;
+                    offset = 3;
+                }
+
+                resp[nroRespuesta] = (char*)malloc(strlen(line + offset) * sizeof(char));
+                strcpy(resp[nroRespuesta],line + offset);
+                nroRespuesta++;
             }
-            nroRespuesta++;
+        } else {
+            //Basura y se ignora. (No es ni Pregunta ni respuesta)
         }
+    }
+    
+    if( nroRespuesta != 4 || respuestasCorrectas != 1) {
+        preguntaValida = 0;
+    }
+
+    //Se agrega la última pregunta del archivo.
+    if (preguntaValida) {
+        agregarPreguntaALista(preg,resp,respuestaCorrecta);
+        cantidadDePreguntas++;
+    }         
+    //Libero los punteros
+    free(preg);
+    for(int i=0; i<4;i++) {
+        free(resp[i]);
     }
 
     fclose(fp);
@@ -95,15 +122,59 @@ void abrirArchivo(){
     }
     printf("Cantidad de Preguntas: %d\n\n", cantidadDePreguntas);
 
-    t_pregunta *pregunta = lista_preguntas;
+    t_pregunta *punteroLista = lista_preguntas;
 
-    while(pregunta != NULL){
-        printf("%s", pregunta->pregunta);
-        printf("%s", pregunta->respuesta1);
-        printf("%s", pregunta->respuesta2);
-        printf("%s", pregunta->respuesta3);
-        printf("%s", pregunta->respuesta4);
-        printf("La correcta es la: %d\n\n", pregunta->rc + 1);
-        pregunta = pregunta->siguiente;
+    int pregu = 1;
+    while(punteroLista != NULL){
+            printf("Pregunta %d: %s",pregu, punteroLista->pregunta);
+            for(int i=0; i<4;i++){
+                printf("Respuesta %d: %s",i+1, punteroLista->respuestas[i]);
+            }
+            printf("\n");
+            pregu++;
+            punteroLista = punteroLista->siguiente;
     }
 }
+
+
+//---------------------------------------------------------------------------------------------------
+/*
+*   Se encarga de agregar las preguntas a la lista.
+*
+*/
+void agregarPreguntaALista(char * pregunta, char * respuestas[], int respuestaCorrecta) {
+    t_pregunta *punteroLista = lista_preguntas;
+    
+    if (punteroLista == NULL) { //Primer Pregunta, lista vacía.
+        
+        //Inicializo la lista
+        punteroLista = malloc(sizeof(t_pregunta));
+        punteroLista->siguiente = NULL;
+        lista_preguntas = punteroLista;
+    } else {
+        //Busco la ultima pregunta
+        t_pregunta *ultimaPregunta = lista_preguntas;
+        while(punteroLista != NULL) {
+            ultimaPregunta = punteroLista;
+            punteroLista = punteroLista->siguiente;
+        }
+        punteroLista = malloc(sizeof(t_pregunta));
+        punteroLista->siguiente = NULL;
+        ultimaPregunta->siguiente = punteroLista;
+    }
+
+    //Cargo la pregunta
+    punteroLista->pregunta = (char*)malloc(strlen(pregunta) * sizeof(char));
+    strcpy(punteroLista->pregunta,pregunta);
+    
+    punteroLista->rc = respuestaCorrecta;
+    
+    //Cargo las respuestas
+    for(int i=0; i<4;i++) {
+        punteroLista->respuestas[i] = (char*)malloc(strlen(respuestas[i]) * sizeof(char));
+        strcpy(punteroLista->respuestas[i],respuestas[i]);
+    }
+}
+
+
+
