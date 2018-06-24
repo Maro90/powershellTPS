250f7dde328cb9e70084ceece9040bd7f7f099cc
Mauro <mauro.d.gonzalez90@gmail.com>
2018-06-24 00:22:28 -0300

    borrando prueba

--- TP3/Ejercicio4/v1/Socket.h
+++
@@ -1,8 +0,0 @@
-#ifndef _SOCKET_H
-#define _SOCKET_H
-
-int leeSocket (int fd, void *datos, int longitud);
-int escribeSocket (int fd, void *datos, int longitud);
-
-
-#endif
--- TP3/Ejercicio4/v1/Socket_Cliente.h
+++
@@ -1,6 +0,0 @@
-#ifndef _SOCKET_CLIENTE_H
-#define _SOCKET_CLIENTE_H
-
-int abreConexion(char *ip, int puerto);
-
-#endif
